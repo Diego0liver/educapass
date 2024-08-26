20beb30c88964bf71b6e3d0ee3fbed92e9ff0490
diego <diegojof95@hotmial.com>
2024-08-26 16:25:21 -0300

    first commit

---
+++ cliente/educapass-client/src/layouts/layoutEscola.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-app-bar
+        color="indigo-darken-4"
+        prominent
+    >
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Educapass - Escola</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <template v-if="$vuetify.display.mdAndUp">
+        <v-btn icon="mdi-account" variant="text"></v-btn>
+        </template>
+    </v-app-bar>
+
+    <v-navigation-drawer
+        v-model="drawer"
+        :location="$vuetify.display.mobile ? 'bottom' : undefined"
+        temporary
+    >
+    <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-view-dashboard" title="Dashboard" :to="{ path: 'Dashboard' }"></v-list-item>
+        <v-list-item prepend-icon="mdi-account-school" title="Alunos" :to="{ path: 'AlunosEscola' }"></v-list-item>
+        <v-list-item prepend-icon="mdi-human-male-board" title="Professores" value="shared"></v-list-item>
+        <v-list-item prepend-icon="mdi-book-open-page-variant" title="Materias" value="starred"></v-list-item>
+        <v-list-item prepend-icon="mdi-table-chair" title="Salas" value="starred"></v-list-item>
+        <v-list-item prepend-icon="mdi-video-vintage" title="Aulas online" value="starred"></v-list-item>
+    </v-list>
+        
+    </v-navigation-drawer>
+
+    <v-main style="height: 500px;">
+        <v-card-text>
+            <slot></slot>
+        </v-card-text>
+    </v-main>
+  </template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+
+  watch: {
+    group () {
+      this.drawer = false
+    },
+  },
+}
+</script>
---
+++ cliente/educapass-client/src/pages/escola/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+    <LayoutEscola>
+        <v-card>
+            <v-card-title>Home Page</v-card-title>
+            <v-card-text>Bem-vindo à página inicial!</v-card-text>
+        </v-card>
+    </LayoutEscola>
+ </template>
+ 
+ <script>
+ import LayoutEscola from '@/layouts/layoutEscola.vue';
+ 
+     export default{
+         components: {
+             LayoutEscola
+         }
+     }
+ </script>
